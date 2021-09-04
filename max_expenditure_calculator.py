467721fe37a54da93a80813396f3ae38ea02a723
Ivan Polukarov <polkarov.i@sch2009.net>
2021-09-05 00:27:42 +0300

    max_expenditure_calculator.py added

---
+++ max_expenditure_calculator.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+PREDS_PATH = 'prediction_at_all.csv'
+
+
+def calculate_max_expendature(year, percent):
+    try:
+        df = pd.read_csv(PREDS_PATH, index_col='Unnamed: 0')
+    except:
+        print(f'File Not found: {PREDS_PATH}')
+        
+    res = df.loc[df['year'] == year, 'value'].values
+    print(res)
+    return {'actual': res, 'plus_percent': (1 + percent / 100) * res}
